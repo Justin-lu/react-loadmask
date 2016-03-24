45b2ff7c82ece55a9fb1135c8446a94237a3aa39
Justin <gdjyluxiaoyong@gmail.com>
2016-03-24 11:19:55 +0800

    update loading

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './style.scss';
+import './style.css';
 import React from 'react';
 
 class LoadMask extends React.Component {
@@ -8,7 +8,16 @@ class LoadMask extends React.Component {
     if (isLoading) {
       return (
         <div className="loading-center" >
-          <div className="loader">Loading...</div>
+          <div id="circularG">
+            <div id="circularG_1" className="circularG"></div>
+            <div id="circularG_2" className="circularG"></div>
+            <div id="circularG_3" className="circularG"></div>
+            <div id="circularG_4" className="circularG"></div>
+            <div id="circularG_5" className="circularG"></div>
+            <div id="circularG_6" className="circularG"></div>
+            <div id="circularG_7" className="circularG"></div>
+            <div id="circularG_8" className="circularG"></div>
+          </div>
         </div>
       );
     }
