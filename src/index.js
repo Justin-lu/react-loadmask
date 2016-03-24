89c3d581627ae11d0637086fef40ea5b77327e37
Justin <gdjyluxiaoyong@gmail.com>
2016-03-24 11:03:47 +0800

    update loading icon

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './style.scss';
+import './style.css';
 import React from 'react';
 
 class LoadMask extends React.Component {
@@ -8,7 +8,16 @@ class LoadMask extends React.Component {
     if (isLoading) {
       return (
         <div className="loading-center" >
-          <div className="loader">Loading...</div>
+          <div id="circularG">
+            <div id="circularG_1" class="circularG"></div>
+            <div id="circularG_2" class="circularG"></div>
+            <div id="circularG_3" class="circularG"></div>
+            <div id="circularG_4" class="circularG"></div>
+            <div id="circularG_5" class="circularG"></div>
+            <div id="circularG_6" class="circularG"></div>
+            <div id="circularG_7" class="circularG"></div>
+            <div id="circularG_8" class="circularG"></div>
+          </div>
         </div>
       );
     }
